88c01a14e06e6e6bfb95cac72279b62fe97a5530
matvenikus <maatvenikus@gmail.com>
2023-09-12 12:39:25 +0300

    reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
-    public int calculate (int money, int months) {
+    public int calculate(int money, int months) {
 
         double percent = 9.99;
         double rate = percent / 100 / 12;
 
         double index = rate + 1;
-        double calcOne = Math.pow (index,months);
+        double calcOne = Math.pow(index, months);
 
         double calcTwo = calcOne - 1;
         double calcThree = rate / calcTwo;
